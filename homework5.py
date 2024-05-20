4bacc76a8e382ca1c127ba1d3f122e30bc12900b
DmitryStitch <torres-1792@mail.ru>
2024-05-20 22:01:29 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['apple', 'orange', 'pineapple', 'melon', 'cherry', 'peach', 'wotermelon']
+print(my_list)
+print(my_list[0], my_list[6])
+print(my_list[3:6])
+my_list[3] = 'kiwi'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'pineapple': 'ананас', 'melon': 'дыня'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict['orange'] = 'цитрус'
+my_dict['cherry']= 'вишня'
+print(my_dict)
