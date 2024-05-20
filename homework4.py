69a4ff79f96f322f89f2758cae99b517a721d595
DmitryStitch <torres-1792@mail.ru>
2024-05-20 20:21:30 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var=('a',2,'b',3,4.5,)
+print(immutable_var)
+#immutable_var[0]=5
+#print(immutable_var) # Потому что символы в картеже не являются частью списка
+mutable_list= ["telephone", "PC", "PS5"]
+print(mutable_list)
+mutable_list[0]="IPhone"
+print(mutable_list)
