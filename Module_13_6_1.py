a7490d68cf4ca7ec8e518d8299356caa4ec0ab69
DmitryStitch <torres-1792@mail.ru>
2024-11-21 00:49:57 +0300

    Add files via upload

--- Module_13_6_1.py
+++ Module_13_6_1.py
@@ -7,14 +7,15 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = ""
-bot = Bot(token = api)
-dp = Dispatcher(bot, storage = MemoryStorage())
 
-kb = InlineKeyboardMarkup(resize_keyboard=True)
-button_1 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories' )
-button_2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
-kb.add(button_1, button_2)
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+inline_kb = InlineKeyboardMarkup(resize_keyboard=True)
+button_расчет = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button_формулы = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+inline_kb.add(button_расчет, button_формулы)
 
 
 class UserState(StatesGroup):
@@ -22,58 +23,63 @@ class UserState(StatesGroup):
     growth = State()
     weight = State()
 
-@dp.message_handler(commands = ['start'])
+
+@dp.message_handler(commands=['start'])
 async def start_massege(message: types.Message):
-    await message.answer("Привет!Я бот помагающий твоему здоровью!")
+    reply_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    button_calc = KeyboardButton(text='Рассчитать')
+    button_info = KeyboardButton(text='Информация')
+    reply_keyboard.add(button_calc, button_info)
+    await message.answer("Привет! Я бот, помогающий вашему здоровью! Выберите 'Рассчитать', чтобы перейти к рассчету", reply_markup=reply_keyboard)
 
 @dp.message_handler(text='Рассчитать')
-async def main_menu(message):
-    await message.answer("Выберите опцию:", reply_markup=kb)
-    await answer()
+async def show_inline_menu(message: types.Message):
+    await message.answer("Выберите опцию:", reply_markup=inline_kb)
 
-@dp.callback_query_handler(text = 'formulas')
+@dp.callback_query_handler(text='formulas')
 async def get_formulas(call):
-    await call.message.answer("10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5")
+    await call.message.answer("10 х вес (кг) + 6,25 х рост (см) – 5 х возраст (лет) + 5")
     await call.answer()
 
-@dp.callback_query_handler(text = ['calories'])
+@dp.callback_query_handler(text='calories')
 async def set_age(call: types.Message):
     await call.message.answer("Введите свой возраст:")
     await UserState.age.set()
 
-@dp.message_handler(state = UserState.age)
+@dp.message_handler(state=UserState.age)
 async def set_growth(message: types.Message, state: FSMContext):
-    await state.update_data(age = int(message.text))
-    await message.answer("Введите свой рост (в см): ")
-    await UserState.next()
+        age = int(message.text)
+        await state.update_data(age=age)
+        await message.answer("Введите свой рост (в см):")
+        await UserState.next()
 
-@dp.message_handler(state = UserState.growth)
+@dp.message_handler(state=UserState.growth)
 async def set_weight(message: types.Message, state: FSMContext):
-    await state.update_data(growth = int(message.text))
-    await message.answer("Введите свой вес (в кг): ")
-    await UserState.next()
+        growth = int(message.text)
+        await state.update_data(growth=growth)
+        await message.answer("Введите свой вес (в кг):")
+        await UserState.next()
 
-@dp.message_handler(state = UserState.weight)
+@dp.message_handler(state=UserState.weight)
 async def send_calories(message: types.Message, state: FSMContext):
-    await state.update_data(weight = int(message.text))
-    data = await state.get_data()
-    age = data.get('age')
-    growth = data.get('growth')
-    weight = data.get('weight')
+        weight = int(message.text)
+        await state.update_data(weight=weight)
+        data = await state.get_data()
+        age = data.get('age')
+        growth = data.get('growth')
+        weight = data.get('weight')
+        calories = 10 * weight + 6.25 * growth - 5 * age + 5
+        await message.answer(f"Ваша норма калорий: {calories}")
+        await state.finish()
 
-    calories = 10 * weight + 6.25 * growth + 5 * age + 5
-
-    await message.answer(f"Ваша норма каллорий:{calories}")
-    await state.finish()
 
 @dp.message_handler(state=None)
-async def all_massages(message: types.Message, state: FSMContext):
-    print(f"Получено сообщение:{message.text}")
+async def all_massages(message: types.Message):
     await message.reply("Введите команду /start, чтобы начать общение.")
+
 
 if __name__ == "__main__":
     try:
         executor.start_polling(dp, skip_updates=True)
     except Exception as e:
-        print(f"Произошла ошибка: {e}")
         logging.exception(e)
