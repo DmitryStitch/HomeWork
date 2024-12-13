796a99183aea09f31570a3c9b34b29ec1ab1f225
DmitryStitch <torres-1792@mail.ru>
2024-12-14 00:23:00 +0300

    Add files via upload

---
+++ homeworkNew.py
@@ -0,0 +1,21 @@
+#1st program
+
+print(9**0.5*5)
+
+#2nd program
+
+x = 9.99 > 9.98 and 1000 != 1000.1
+print(x)
+
+#3rd program
+x = 2 * 2 + 2
+y = 2 * (2 + 2)
+z = x == y
+print(z)
+
+#4th program
+
+x = "123.456"
+y = int(float("123.456")*10)
+z = y % 10
+print(z)
